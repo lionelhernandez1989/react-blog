29797a648a5485fe933ee75c742d9713fb0cb013
Samuel <samuel@127.17.10.128>
2022-10-31 23:59:26 -0700

    1

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import {Helmet} from "react-helmet";
+
+const Home = () => {
+    return(
+        <>
+            <Helmet>
+                <title>
+                    هشدار گوگل به کاربران مک برای به&zwnj;روزرسانی کروم و رفع مشکل امنیتی این مرورگر  - زومیت
+                </title>
+                <meta property="og:description"
+                      content="گوگل یک به روزرسانی امنیتی جدید برای نسخه مک مرورگر گوگل کروم منتشر کرد."/>
+                <meta property="og:title"
+                      content="هشدار گوگل به کاربران مک برای به&zwnj;روزرسانی کروم و رفع مشکل امنیتی این مرورگر  - زومیت"/>
+                <meta property="og:url"
+                      content="https://www.zoomit.ir/software-application/387488-google-high-risk-chrome-security-flaw/"/>
+                <meta property="og:site_name" content="زومیت"/>
+            </Helmet>
+        </>
+    )
+}
+
+export default Home;
